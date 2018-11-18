f68bd0a7ad7f5c28f516761e2d7ed5260fe6f30a
gattonero1052 <gattonero1052@gmail.com>
2018-11-18 22:16:46 +0800

    edit

--- java-project/src/main/java/com/mskf/ListFunc.java
+++ java-project/src/main/java/com/mskf/ListFunc.java
@@ -1,12 +1,13 @@
 package com.mskf;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by mskf on 2018/11/18.
  */
 public class ListFunc {
-    public static java.util.List create(int[] array){
-        return Arrays.asList(array);
+    public static List<Integer> create(int[] array){
+        return Arrays.asList(1,2,3);
     }
 }
